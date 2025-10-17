4f87adf2f5b19e8afce53b869634f180376d362e
Dominic-Um <dumbrasa@ucsc.edu>
2025-10-17 02:28:50 +0000

    Step 2: Co-Authored by: aaychen@ucsc.edu

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,71 @@
-
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 document.body.append(canvas);
+
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+// 👇 add type annotations so TS knows what these are
+type Point = { x: number; y: number };
+const lines: Point[][] = [];
+const redoLines: Point[][] = [];
+
+let currentLine: Point[] | null = null;
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+
+  currentLine = [];
+  lines.push(currentLine);
+  redoLines.splice(0, redoLines.length);
+  currentLine.push({ x: cursor.x, y: cursor.y });
+
+  redraw();
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active && currentLine) {
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+    currentLine.push({ x: cursor.x, y: cursor.y });
+
+    redraw();
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+  currentLine = null;
+
+  redraw();
+});
+
+function redraw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (const line of lines) {
+    if (line.length > 1) {
+      ctx.beginPath();
+      const { x, y } = line[0];
+      ctx.moveTo(x, y);
+      for (const point of line) {
+        ctx.lineTo(point.x, point.y);
+      }
+      ctx.stroke();
+    }
+  }
+}
+
+document.body.append(document.createElement("br"));
+
+const clearButton = document.createElement("button");
+clearButton.textContent = "Clear";
+document.body.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  lines.splice(0, lines.length);
+  redraw();
+});
